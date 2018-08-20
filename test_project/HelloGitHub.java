2e14b362608cb135061b88c72df657bfefe365ba
Deep-Joe <wy_ycd@163.com>
2018-08-20 13:44:44 +0800

    Add a Folder
    This has a java file to test

---
+++ test_project/HelloGitHub.java
@@ -0,0 +1,9 @@
+public class HelloGitHub {
+	public static void main(String[] args) {
+		System.out.println("HelloGitHub,Nice to see you!!");
+		metho01();
+	}
+	public static void metho01() {
+		System.out.println("this is a test to modify.");
+	}
+}
